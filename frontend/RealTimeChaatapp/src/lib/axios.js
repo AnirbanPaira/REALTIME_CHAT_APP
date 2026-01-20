9293e2978f9b2663ac0292d7f50c3a1d42ad1f59
Anirban Paira <pairaanirban2002@gmail.com>
2026-01-20 16:45:17 +0530

    fix

--- frontend/RealTimeChaatapp/src/lib/axios.js
+++ frontend/RealTimeChaatapp/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const API_URL = import.meta.env.VITE_API_URL || "https://realtime-chatapp-backend-c6sg.onrender.com";
 
 export const axiosInstance = axios.create({
     baseURL:`${API_URL}/api`,
